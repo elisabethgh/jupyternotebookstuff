96f45de62c5d528b0e057fd79984cde9f1217806
elisabethgh <108864927+elisabethgh@users.noreply.github.com>
2022-07-26 12:56:35 -0700

    Add files via upload

---
+++ Notebook for tools for Data Science.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter notebook on IBM Watson Studio
+# 
+
+# **Ellie Heldridge**
+# Student
+
+# *I am interested in Data science because it is important for me to be able to graph data from our telescope. It is a field that I am interested in for the future.*
+
+# ### My code should do the math problem 11*11
+
+# In[2]:
+
+
+a=11*11
+print(a)
+
+
+# ***
+# `Here are some things about me!`
+# ***
+# * I am a high shool student who lives in seattle and goes to school in california
+# * I am currently doing exoplanet research with my school
+# * I also show horses competitively
+# 
